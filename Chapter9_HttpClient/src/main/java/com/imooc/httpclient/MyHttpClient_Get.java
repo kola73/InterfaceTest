7ed31646f3a5ae5a5728789bd2dd7298127864c5
kola <282156710@qq.com>
2018-04-30 20:56:19 +0800

    httpclient框架

---
+++ Chapter9_HttpClient/src/main/java/com/imooc/httpclient/MyHttpClient_Get.java
@@ -0,0 +1,27 @@
+package com.imooc.httpclient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+//get请求的实现
+public class MyHttpClient_Get {
+    @Test
+    public void getTest() throws IOException {
+//        用来存放结果
+        String result;
+//        创建get方法
+        HttpGet get = new HttpGet("http://www.baidu.com");
+//        执行get方法
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = httpClient.execute(get);
+//        获取整个响应的全部内容，并把它转化为字符串(为了防止乱码，加上编码格式utf-8
+        result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+
+    }
+}
